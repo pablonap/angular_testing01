b8f708f0782fcc3c02076126a22075a5112e3871
chipo <pnapoli33@gmail.com>
2021-10-12 18:01:50 -0300

    Use "waitForAsync" in order to execute the test only after the promise gets evaluated and the body from then() method finishes.

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { CoursesCardListComponent } from "./courses-card-list.component";
 import { CoursesModule } from "../courses.module";
 import { COURSES } from "../../../../server/db-data";
@@ -12,19 +12,22 @@ describe("CoursesCardListComponent", () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [CoursesModule],
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [CoursesModule],
+      })
+        .compileComponents()
+        .then(() => {
+          fixture = TestBed.createComponent(CoursesCardListComponent);
+          component = fixture.componentInstance;
+        });
     })
-      .compileComponents()
-      .then(() => {
-        fixture = TestBed.createComponent(CoursesCardListComponent);
-        component = fixture.componentInstance;
-      });
-  });
+  );
 
   it("should create the component", () => {
     expect(component).toBeTruthy();
+    console.log("something here");
   });
 
   it("should display the course list", () => {
