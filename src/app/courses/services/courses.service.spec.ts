7f434f47954edc32d9610b7544264ef4e65be559
chipo <pnapoli33@gmail.com>
2021-10-12 14:40:54 -0300

    Add "should save the course data" spec

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -5,6 +5,8 @@ import {
   HttpTestingController,
 } from "@angular/common/http/testing";
 import { COURSES } from "../../../../server/db-data";
+import { Course } from "../model/course";
+import { request } from "http";
 
 describe("CoursesService", () => {
   let coursesService: CoursesService,
@@ -52,5 +54,24 @@ describe("CoursesService", () => {
     expect(req.request.method).toEqual("GET");
 
     req.flush(COURSES[12]);
+  });
+
+  it("should save the course data", () => {
+    const changes: Partial<Course> = {
+      titles: { description: "Testing Course" },
+    };
+    coursesService.saveCourse(12, changes).subscribe((course) => {
+      expect(course.id).toBe(12);
+    });
+
+    const req = httpTestingController.expectOne("/api/courses/12");
+    expect(req.request.method).toEqual("PUT");
+    expect(req.request.body.titles.description).toEqual(
+      changes.titles.description
+    );
+    req.flush({
+      ...COURSES[12],
+      ...changes,
+    });
   });
 });
