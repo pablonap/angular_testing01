10dcd8c03f288b458fe64d592914cde809c3db93
chipo <pnapoli33@gmail.com>
2021-10-12 14:27:25 -0300

    Add spyOn to the first spec in order to assert that the log method from the logger object have been called just once.

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,10 +3,14 @@ import { LoggerService } from "./logger.service";
 
 describe("CalculatorService", () => {
   it("should add two numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = new LoggerService();
+    spyOn(logger, "log");
+    const calculator = new CalculatorService(logger);
+
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it("should substract two numbers", () => {
