2a44ef83c109e014f2d5dd90241ea0993ce45ee5
chipo <pnapoli33@gmail.com>
2021-10-12 14:38:22 -0300

    Make assertions in "should retrieve all courses" spec by injecting CoursesService and HttpTestingController

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -4,6 +4,7 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from "@angular/common/http/testing";
+import { COURSES } from "../../../../server/db-data";
 
 describe("CoursesService", () => {
   let coursesService: CoursesService,
@@ -22,6 +23,18 @@ describe("CoursesService", () => {
   });
 
   it("should retrieve all courses", () => {
-    pending();
+    coursesService.findAllCourses().subscribe((courses) => {
+      expect(courses).toBeTruthy("No courses returned");
+      expect(courses.length).toBe(12, "incorrect number of courses");
+
+      const course = courses.find((course) => course.id == 12);
+
+      expect(course.titles.description).toBe("Angular Testing Course");
+    });
+
+    const req = httpTestingController.expectOne("/api/courses");
+    expect(req.request.method).toEqual("GET");
+
+    req.flush({ payload: Object.values(COURSES) });
   });
 });
