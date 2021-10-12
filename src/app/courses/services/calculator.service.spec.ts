66e7e08f48d0323d26141888e1f2c1150c97a3c3
chipo <pnapoli33@gmail.com>
2021-10-12 14:29:44 -0300

    Add "beforeEach" method in order to initialize the mock and the service under test before each test is run

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,24 +1,27 @@
 import { CalculatorService } from "./calculator.service";
-import { LoggerService } from "./logger.service";
+
+let calculator: CalculatorService, loggerSpy: any;
+
+beforeEach(() => {
+  loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
+  calculator = new CalculatorService(loggerSpy);
+});
 
 describe("CalculatorService", () => {
   it("should add two numbers", () => {
     // const logger = new LoggerService();
     // spyOn(logger, "log");
 
-    const logger = jasmine.createSpyObj("LoggerService", ["log"]);
-    const calculator = new CalculatorService(logger);
-
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it("should substract two numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
     const result = calculator.subtract(2, 2);
 
     expect(result).toBe(0);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
