4ab5f6e965459e8a449939f88e19498302b0301f
chipo <pnapoli33@gmail.com>
2021-10-12 14:34:12 -0300

    Create new test suit called "courses.service.spec.ts"; use "HttpClientTestingModule" to create a mock from HttpClient an inject into the service; define "HttpTestingController" in order to retrieve mock data.

---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from "@angular/core/testing";
+import { CoursesService } from "./courses.service";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+
+describe("CoursesService", () => {
+  let coursesService: CoursesService,
+    httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CoursesService],
+    });
+
+    coursesService = TestBed.inject<CoursesService>(CoursesService);
+    httpTestingController = TestBed.inject<HttpTestingController>(
+      HttpTestingController
+    );
+  });
+
+  it("should retrieve all courses", () => {
+    pending();
+  });
+});
